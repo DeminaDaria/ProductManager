6639f3aff80ddcea8e00bb8a89a2a8c0537993f7
Demina Daria <demina.dasha92@gmail.com>
2020-10-29 17:01:08 +0300

    Update ProductManager.java

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -23,7 +23,7 @@ public class ProductManager {
         Product[] result = new Product[0];
 
         for (Product product : repository.getAll()) {
-            if (matches(product, text)) {
+            if (product.matches(text)) {
 
                 Product[] tmp = new Product[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
@@ -32,33 +32,5 @@ public class ProductManager {
             }
         }
     return result;
-    }
-
-    public boolean matches(Product product, String search) {
-
-        if (product instanceof Book) {
-            Book book = (Book) product;
-
-            if (book.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-
-            if (book.getAuthor().equalsIgnoreCase(search)) {
-                return true;
-            }
-        }
-
-        if (product instanceof Smartphone) {
-            Smartphone smartphone = (Smartphone) product;
-
-            if (smartphone.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-
-            if (smartphone.getMaker().equalsIgnoreCase(search)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
